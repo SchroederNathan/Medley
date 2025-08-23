c7070e713b901646ca7d2547d4ab1c23b863fad1
“Nathan <schroedernathan2011@icloud.com>
2025-08-23 17:00:40 -0400

    Setup expo project with basic supabase integration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import "react-native-url-polyfill/auto";
+import Auth from "./auth";
+import { supabase } from "./lib/utils";
 
 export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
