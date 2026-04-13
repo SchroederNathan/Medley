eb3f897384f31ff55f8b231154beab441fe74ed5
“Nathan <schroedernathan2011@icloud.com>
2026-04-12 20:16:24 -0400

    Cleaned up home screen spacing

--- app/(protected)/(tabs)/(home)/index.tsx
+++ app/(protected)/(tabs)/(home)/index.tsx
@@ -1,14 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import Svg, {
-  Defs,
-  FeBlend,
-  FeFlood,
-  FeGaussianBlur,
-  Filter,
-  Path,
-} from "react-native-svg";
 import { AnimatedBlur } from "../../../../components/ui/animated-blur";
 import { AnimatedChevron } from "../../../../components/ui/animated-chevron";
 import Carousel from "../../../../components/ui/carousel";
@@ -98,42 +90,6 @@ const IndexScreen = () => {
 
   return (
     <View style={[styles.container, { backgroundColor: theme.background }]}>
-      <Svg
-        width="150%"
-        height="100%"
-        viewBox="0 0 500 550"
-        style={styles.spotlightSvg}
-      >
-        <Defs>
-          <Filter
-            id="filter0_f_2_34"
-            x="-167.2"
-            y="-262.2"
-            width="700.02"
-            height="850.854"
-            filterUnits="userSpaceOnUse"
-          >
-            <FeFlood floodOpacity="0" result="BackgroundImageFix" />
-            <FeBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="BackgroundImageFix"
-              result="shape"
-            />
-            <FeGaussianBlur
-              stdDeviation="61.85"
-              result="effect1_foregroundBlur_2_34"
-            />
-          </Filter>
-        </Defs>
-        <Path
-          d="M-43.5 -81.5L7.5 -138.5L420.12 380.955L280.62 480.954L-43.5 -81.5Z"
-          fill="#D4D4D4"
-          fillOpacity="0.1"
-          filter="url(#filter0_f_2_34)"
-        />
-      </Svg>
-
       <HomeBackdrop
         media={popularMovies.data ?? []}
         currentIndex={carouselIndex}
@@ -156,7 +112,7 @@ const IndexScreen = () => {
           {favoriteRecommendations.data &&
           favoriteRecommendations.data.length > 0 ? (
             <Carousel
-              style={{ marginTop: 0 }}
+              style={{ marginTop: 20 }}
               media={favoriteRecommendations.data}
               title="Based on your favorites"
             />
